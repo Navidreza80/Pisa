f7b96885b425b3799ba09a216507de55faddabfd
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-04-22 19:44:46 +0330

    Hero section + offers section

--- src/components/home-container/home-container.tsx
+++ src/components/home-container/home-container.tsx
@@ -1,9 +1,11 @@
+import Offers from "./offers/offers";
 import HeroSection from "./hero-section/hero-section";
 
 export default async function HomeContainer() {
   return (
     <div>
       <HeroSection />
+      <Offers />
     </div>
   );
 }
