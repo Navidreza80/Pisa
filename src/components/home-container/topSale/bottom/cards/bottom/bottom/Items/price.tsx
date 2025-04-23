44b4a6a3a73099b6d6583947883960aff2affac9
taha <taha2010talebi@gmail.com>
2025-04-23 19:27:16 +0330

    font and fix bug

--- src/components/home-container/topSale/bottom/cards/bottom/bottom/Items/price.tsx
+++ src/components/home-container/topSale/bottom/cards/bottom/bottom/Items/price.tsx
@@ -7,7 +7,7 @@ function Price({ price }: PriceProps) {
   return (
     <div className='relative'>
       <div className='flex flex-row-reverse gap-[5px] '>
-        <h1 className='text-[20px] font-[700] my-auto'>{price}</h1>
+        <h1 className='text-[20px] font-[yekannum] font-[700] my-auto'>{price}</h1>
         <p className='text-[12px] text-[#595959] font-[700] my-auto'>تومان</p>
       </div>
     </div>
--- src/components/home-container/topSale/top/left/left.tsx
+++ src/components/home-container/topSale/top/left/left.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Left() {
   return (
-    <div className='text-white rounded-2xl bg-[#586CFF] py-[16px] px-[9px]'>مشاهده همه</div>
+    <div className='text-white my-[auto] h-[58px] rounded-2xl bg-[#586CFF] py-[16px] px-[9px]'>مشاهده همه</div>
   )
 }
 
