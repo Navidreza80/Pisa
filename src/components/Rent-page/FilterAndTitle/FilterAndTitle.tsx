0d5563d053b6649f8362a532a62b9bd85f317d26
taha <taha2010talebi@gmail.com>
2025-05-01 12:58:13 +0330

    about us  (darkmode + 4 languages) rent(design)

---
+++ src/components/Rent-page/FilterAndTitle/FilterAndTitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from './Title/Title'
+import Filter from './Filter/Filter'
+
+function FilterAndTitle() {
+  return (
+    <div>
+        <Title />
+        <Filter />
+    </div>
+  )
+}
+
+export default FilterAndTitle
