be0f41792c31aa2ab110149b5cddc04d7e00ebde
taha <taha2010talebi@gmail.com>
2025-05-09 17:26:50 +0330

    house-detail(yard 3D)

--- src/components/houseDetail/RightImg.tsx
+++ src/components/houseDetail/RightImg.tsx
@@ -11,7 +11,8 @@ function RightImg({ detail }) {
         { title: "پذیرایی", url: 'https://static.vecteezy.com/system/resources/previews/019/062/597/non_2x/full-spherical-seamless-hdri-360-panorama-in-interior-of-guest-living-room-hall-in-apartment-with-sofa-armchairs-and-dinner-table-in-equirectangular-projection-vr-content-photo.jpg' },
         { title: "اتاق", url: 'https://static.vecteezy.com/system/resources/previews/022/876/112/non_2x/full-seamless-spherical-hdri-360-panorama-in-interior-of-bedroom-in-studio-apartments-with-arched-access-to-the-balcony-in-equirectangular-projection-vr-content-photo.jpg' },
         { title: "حمام", url: 'https://static.vecteezy.com/system/resources/previews/019/924/442/non_2x/white-seamless-360-hdri-panorama-in-interior-of-expensive-bathroom-in-modern-flat-apartments-with-washbasin-in-equirectangular-projection-with-zenith-and-nadir-vr-ar-content-photo.jpg' },
-        { title: "پارکینگ", url: 'https://cdn.polyhaven.com/asset_img/primary/parking_garage.png?height=760' }
+        { title: "پارکینگ", url: 'https://cdn.polyhaven.com/asset_img/primary/parking_garage.png?height=760' },
+        { title: "حیاط", url: 'https://static.vecteezy.com/system/resources/previews/028/675/380/large_2x/360-hdri-panorama-view-in-garden-with-flower-bed-yard-near-wooden-eco-house-in-village-in-equirectangular-spherical-projection-for-vr-ar-content-photo.jpg' }
     ];
 
     const handle3DImageChange = (title) => {
