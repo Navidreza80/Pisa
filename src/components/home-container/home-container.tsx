b11c03849def4cac09b252e5aac814d3601a730f
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-05-14 19:53:55 +0330

    Test AI house recommendation

--- src/components/home-container/home-container.tsx
+++ src/components/home-container/home-container.tsx
@@ -7,8 +7,6 @@ import HeroSection from "./hero-section/hero-section";
 import Popular from "./locations/locations";
 import Offers from "./offers/offers";
 import TopSales from "./topSale/TopSale";
-// import Comments from "./comments/Comments";
-import AIAssistant from "./ai-assistant/ai-assistant";
 
 export default async function HomeContainer() {
   const data: HouseItemsInterface[] = await fetchHouses({
@@ -19,7 +17,7 @@ export default async function HomeContainer() {
   const esfahanHouses = data.filter((e) =>
     e.address?.includes("اصفهان")
   ).length;
-  const houseLength = data.length;
+  // const houseLength = data.length;
   return (
     <Container>
       <div className="flex flex-col flex-wrap gap-[118px] w-[85.5%]">
