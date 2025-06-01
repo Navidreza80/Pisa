73c438649c56033442257bcfbc37a7ac57c82084
NavidReza <navidrezaabbaszazdeh89@gmail.com>
2025-06-01 19:57:56 +0330

    Bind my reserves to API

--- src/utils/helper/format-date.ts
+++ src/utils/helper/format-date.ts
@@ -1,3 +1,28 @@
+import moment from "moment-jalaali";
+
 export const formatDate = (date: Date): string => {
   return new Intl.DateTimeFormat("fa-IR").format(date);
 };
+
+moment.locale("fa", {
+  jMonths: [
+    "فروردین",
+    "اردیبهشت",
+    "خرداد",
+    "تیر",
+    "مرداد",
+    "شهریور",
+    "مهر",
+    "آبان",
+    "آذر",
+    "دی",
+    "بهمن",
+    "اسفند",
+  ],
+});
+moment.locale("fa"); // فعال‌سازی locale فارسی
+
+export default function formatToPersianDate(date) {
+  const persianDate = moment(date);
+  return persianDate.format("jD jMMMM jYYYY / HH:mm");
+}
