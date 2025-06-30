4cbdbc62557f0489d850dcb11bdfbda2a9668fca
taha <taha2010talebi@gmail.com>
2025-06-30 17:32:51 +0330

    AddPropertyStepFour 4 languages

--- src/components/pages/sd-property-create/contents/StepFour.tsx
+++ src/components/pages/sd-property-create/contents/StepFour.tsx
@@ -1,26 +1,28 @@
 "use client";
 import InputText from "@/components/common/inputs/text-input-with-label";
+import { useTranslations } from 'next-intl';
 
 const AddPropertyStepFour = ({ setPhotoURL, photoURL, handleChange }) => {
+  const t = useTranslations('AddPropertyStepFour');
+
   return (
     <div className="mt-8 flex flex-col justify-center items-center">
       <div className="flex flex-row-reverse justify-start flex-wrap text-[20px] font-bold w-full">
         <h1 className="text-base font-medium mb-[19px] w-full text-text-secondary">
-          تصاویر ملک
+          {t('title')}
         </h1>
-        <h2 className="text-primary">.یک تصویر بهتر از هزار کلمه</h2>
+        <h2 className="text-primary">{t('subtitle1')}</h2>
         <h2 className="text-text font-semibold">
-          .با قرار دادن عکس شانس دیده شدن ملک‌تان را ۵ برابر کنید
+          {t('subtitle2')}
         </h2>
       </div>
       <div
-        dir="rtl"
         className="mt-[81px] grid grid-cols-2 gap-[30px] justify-center"
       >
         {photoURL.map((val, i) => (
           <InputText
             key={i}
-            label={`لینک عکس:`}
+            label={t('imageLinkLabel')}
             value={val}
             onChange={(e) => handleChange(i, e.target.value)}
           />
@@ -29,4 +31,5 @@ const AddPropertyStepFour = ({ setPhotoURL, photoURL, handleChange }) => {
     </div>
   );
 };
+
 export default AddPropertyStepFour;
