39d73467b2c5fcae7b50b1b31d05014797572e85
taha <taha2010talebi@gmail.com>
2025-07-23 22:38:55 +0330

    user picture update

--- src/components/pages/bd-edit-profile/container/index.tsx
+++ src/components/pages/bd-edit-profile/container/index.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useUser } from "@/utils/service/user/get";
 import { useEditUser } from "@/utils/service/user/put";
+import { useUploadPicture } from "@/utils/service/user/uploadUserPicture";
 import { useFormik } from "formik";
 import { useTranslations } from "next-intl";
 import { useState } from "react";
@@ -13,15 +14,18 @@ import { useState } from "react";
 export default function BuyerInformation() {
   const t = useTranslations("UserInformation");
   const { mutate } = useEditUser();
+  const { mutate: uploadPicture } = useUploadPicture();
   const { data } = useUser();
   const [password, setPassword] = useState("");
+
   const formik = useFormik({
     initialValues: {
-      firstName: data?.firstName,
-      lastName: data?.lastName,
-      email: data?.email,
-      phoneNumber: data?.phoneNumber,
+      firstName: data?.firstName || "",
+      lastName: data?.lastName || "",
+      email: data?.email || "",
+      phoneNumber: data?.phoneNumber || "",
     },
+    enableReinitialize: true,
     onSubmit: (values) => {
       mutate(values);
     },
@@ -32,32 +36,28 @@ export default function BuyerInformation() {
       name: "firstName",
       placeHolder: t("formFields.firstName"),
       value: formik.values.firstName,
-      onChange: formik.handleChange,
     },
     {
       name: "lastName",
       placeHolder: t("formFields.lastName"),
       value: formik.values.lastName,
-      onChange: formik.handleChange,
     },
     {
       name: "email",
       placeHolder: t("formFields.email"),
       value: formik.values.email,
-      onChange: formik.handleChange,
     },
     {
       name: "phoneNumber",
       placeHolder: t("formFields.phoneNumber"),
       value: formik.values.phoneNumber,
-      onChange: formik.handleChange,
     },
   ];
 
   return (
     <ContainerDashboard>
       <form onSubmit={formik.handleSubmit}>
-        <div className="mb-8 text-center md:">
+        <div className="mb-8 text-center">
           <h2 className="text-text text-[20px] font-extrabold font-yekan">
             {t("profileImage.title")}
           </h2>
@@ -69,13 +69,15 @@ export default function BuyerInformation() {
               type="file"
               id="file-upload"
               className="hidden"
+              accept="image/*"
               onChange={(e) => {
                 const file = e.target.files?.[0];
                 if (file) {
-                  formik.setFieldValue("profilePicture", file);
+                  uploadPicture(file);
                 }
               }}
             />
+
             <label
               htmlFor="file-upload"
               className="w-[25px] h-[25px] rounded-full bg-primary cursor-pointer flex justify-center absolute top-[15%] right-0 border-background border-[2px]"
@@ -89,7 +91,7 @@ export default function BuyerInformation() {
 
         <div className="h-[1px] bg-border" />
         <div className="my-8 flex flex-col md:flex-row-reverse justify-between gap-6 md:gap-0">
-          <div className="text-center md:">
+          <div className="text-center">
             <h2 className="text-text text-[20px] font-extrabold font-yekan ">
               {t("personalInfo.title")}
             </h2>
@@ -106,26 +108,24 @@ export default function BuyerInformation() {
               <Button variant="ghost">{t("buttons.cancel")}</Button>
             </div>
           </div>
+
           <div className="flex flex-col flex-wrap gap-4 w-full md:w-[calc(55%)]">
-            {inputs.map((item) => {
-              return (
-                <Input
-                  defaultValue={item.value}
-                  key={item.name}
-                  name={item.name}
-                  placeholder={item.placeHolder}
-                  className="h-12 placeholder:text-text-secondary md:w-[calc(65%)] w-full placeholder:text-[16px] border-border border-[2px] px-5 rounded-2xl"
-                  value={item.value}
-                  onChange={formik.handleChange}
-                />
-              );
-            })}
+            {inputs.map((item) => (
+              <Input
+                key={item.name}
+                name={item.name}
+                placeholder={item.placeHolder}
+                className="h-12 placeholder:text-text-secondary md:w-[calc(65%)] w-full placeholder:text-[16px] border-border border-[2px] px-5 rounded-2xl"
+                value={item.value}
+                onChange={formik.handleChange}
+              />
+            ))}
           </div>
         </div>
 
         <div className="h-[1px] bg-border" />
         <div className="my-8 flex flex-col md:flex-row-reverse justify-between gap-6 md:gap-0">
-          <div className="text-center md:">
+          <div className="text-center">
             <h2 className="text-text text-[20px] font-extrabold font-yekan ">
               {t("security.title")}
             </h2>
@@ -134,7 +134,7 @@ export default function BuyerInformation() {
             </p>
             <div className="mt-4 flex justify-center md:justify-end gap-2">
               <Button
-                onClick={() => mutate({ password: password })}
+                onClick={() => mutate({ password })}
                 className="bg-primary cursor-pointer rounded-xl hover:bg-primary text-white"
               >
                 {t("buttons.applyChanges")}
